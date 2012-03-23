4889555feeb9dbb99605b41c959254b54cfeb31a
yalab <rudeboyjet@gmail.com>
2012-03-23 20:13:17 +0900

    First commit.

---
+++ lib/tasks/csv_rails_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :csv_rails do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '6f2d28438a58023c9b1a016e1d4c83bf5afdcdc92e1e2467517ad9bf2844152ebc4ecce1fd37bb32be7760e1a66efb48db0adcebb3f22594783ec79659147fb1'
