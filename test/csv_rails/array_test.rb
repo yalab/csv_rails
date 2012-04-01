bbda51e1c8d9e47766b4f68c8736bf1304352b7c
yalab <rudeboyjet@gmail.com>
2012-04-02 08:23:01 +0900

    Refactoring array_test using setup.

--- test/csv_rails/array_test.rb
+++ test/csv_rails/array_test.rb
@@ -2,18 +2,24 @@
 require 'test_helper'
 
 class CsvRails::ArrayTest < ActiveSupport::TestCase
+  setup do
+    I18n.locale = :ja
+    @csv_rails_scope = {name: "名前", age: "年齢"}
+    I18n.backend.store_translations(:ja, :csv_rails => @csv_rails_scope)
+  end
+
+  teardown do
+    I18n.locale = :en
+  end
+
   test ".to_csv with header option" do
     header = ['foo', 'bar']
     assert_equal  header.join(','), [].to_csv(:header => header).chomp
   end
 
   test ".to_csv using I18n csv_rails scope" do
-    I18n.locale = :ja
-    translations = {name: "名前", age: "年齢"}
-    I18n.backend.store_translations(:ja, :csv_rails => translations)
-    fields = translations.keys
-    assert_equal translations.values.join(','), [].to_csv(:fields => fields).chomp
-    I18n.locale = :en
+    fields = @csv_rails_scope.keys
+    assert_equal @csv_rails_scope.values.join(','), [].to_csv(:fields => fields).chomp
   end
 
   test ".to_csv accept encoding" do
