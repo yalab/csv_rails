893b77e4296109068d31f359ecd6d9b2bae4e0e2
yalab <rudeboyjet@gmail.com>
2012-04-05 18:35:36 +0900

    Fixed for rails 3.0.12.

--- lib/csv_rails/active_record.rb
+++ lib/csv_rails/active_record.rb
@@ -4,6 +4,15 @@ module CsvRails
       base.extend(CsvRails::ActiveModel::ClassMethods)
       base.send(:include, CsvRails::ActiveModel::InstanceMethods)
       ::ActiveRecord::Relation.send(:include, CsvRails::ActiveModel::ClassMethods)
+      unless base.respond_to?(:attribute_names)
+        base.extend(ClassMethods) # for rails 3.0.12
+      end
+    end
+
+    module ClassMethods
+      def attribute_names
+        column_names.map(&:to_sym)
+      end
     end
   end
 end
