c826373389259e5c37a4f3c4e27c37c13a766fad
yalab <rudeboyjet@gmail.com>
2012-08-11 13:52:52 +0900

    Fixed mongoid config.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,4 +21,4 @@ Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+
