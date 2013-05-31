bbb96273e4fd8793b6fe279ae38b0f9030e18075
yalab <rudeboyjet@gmail.com>
2013-05-31 16:58:25 +0900

    Refactoring.

--- lib/csv_rails/import.rb
+++ lib/csv_rails/import.rb
@@ -3,27 +3,27 @@ module CsvRails::Import
   module ClassMethods
     def csv_import(body, opts={})
       fields = opts[:fields]
-      objects = []
+      records = []
       CSV.parse(body).each do |row|
         unless fields
           fields = row
           next
         end
         attributes = ActiveSupport::HashWithIndifferentAccess.new(Hash[fields.zip(row)])
-        object = if id = attributes['id']
+        record = if id = attributes['id']
                    self.where(id: id).first_or_initialize
                  else
                    self.new
                  end
-        object.attributes = attributes
+        record.attributes = attributes
         if block_given?
-          val = yield object, attributes
+          val = yield record, attributes
           next if val == false
         end
-        object.save!
-        objects << object
+        record.save!
+        records << record
       end
-      objects
+      records
     end
   end
 end
